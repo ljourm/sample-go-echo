10abb4ca390a1157480f8fbbadeccbcd992b3ece
ljourm <ljourm@gmail.com>
2023-02-27 01:31:07 +0900

    feat: envによってローカルとlambdaの起動方法を変更

--- main.go
+++ main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"context"
+	"log"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	echoAdapter "github.com/awslabs/aws-lambda-go-api-proxy/echo"
+	"github.com/joho/godotenv"
 )
+
+func initEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Print(".env is not found")
+	}
+}
 
 func Handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
 	router := NewRouter()
@@ -17,8 +27,12 @@ func Handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.AP
 }
 
 func main() {
-	// router := NewRouter()
-	// router.Logger.Fatal(router.Start(":1323"))
+	initEnv()
 
-	lambda.Start(Handler)
+	if(os.Getenv("IS_LOCAL") == "true") {
+		router := NewRouter()
+		router.Logger.Fatal(router.Start(":1323"))
+	} else {
+		lambda.Start(Handler)
+	}
 }
